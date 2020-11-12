6e634942fbba7d78490df9e39bb55d5cb1a281ea
D3VoS <31460619+D3VoS@users.noreply.github.com>
2020-11-12 14:16:31 +0000

    Update settings.py
    removed heroku from django, just forget I even bothered

--- ampla/settings.py
+++ ampla/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from decouple import config
-import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -135,5 +134,3 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())

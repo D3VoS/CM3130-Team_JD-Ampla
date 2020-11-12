4a4d22559766ed8774c22040ee080f1ae073d3a7
D3VoS <31460619+D3VoS@users.noreply.github.com>
2020-11-12 14:13:32 +0000

    Update settings.py
    Added heroku to django

--- ampla/settings.py
+++ ampla/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from decouple import config
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -133,3 +134,6 @@ STATICFILES_DIRS = [
 # MEDIA FILES / Where any uploaded files will be stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())

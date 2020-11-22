b1da16e1bf6ee82fbe61abcf7970c812a47d8d0a
osmith1998 <1502307@rgu.ac.uk>
2020-11-22 17:44:48 +0000

    For the most part the calendar is done, need to make some db queries to grab data

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
     var d = new Date();
     var currentMonth = d.getMonth(); // Actual Month
     var currentYear = d.getFullYear(); // Actual Year
+    var currentDate = d.getDate();
+
     let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     let months = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"];
@@ -16,52 +18,66 @@ $(document).ready(function(){
         return new Date(year, month, 1).getDay();
     }
 
-
-
     var calculatedMonth = currentMonth; //Extra Months
     var calculatedYear = currentYear; //Extra Years
     
-
     function getHeading(){
         $("#heading").html(`${months[calculatedMonth] + " " + calculatedYear}`) // Creates the title
     }
     
     function getCalendar(){
         $(".calendarDates").empty();
+
+        for (let weekday = 1; weekday <= getFirstDayOfMonth(calculatedMonth, calculatedYear); weekday++){
+            $(".calendarDates").append(`<div class = "spacer"></div>`);
+        }
         
         for (let day = 1; day <= getNoOfDaysInMonth(calculatedMonth, calculatedYear); day++){
-            $(".calendarDates").append(`<div class = "day">${day}</div>`);
+
+            if (currentDate == day && currentMonth == calculatedMonth && currentYear == calculatedYear){
+                $(".calendarDates").append(`<div class = "day" id = "today" >${day}</div>`); //Marked for today for separate css 
+            }
+            else{
+                $(".calendarDates").append(`<div class = "day">${day}</div>`);
+            }
+            
         }
     }
     
     getHeading();
     getCalendar();
+
+    var deviation = 0;
     
     $("#next").click(function(){
         
-        if (calculatedMonth >= 11){ // If the person goes over 12 months, it adds 1 to the year and resets the month
+        if (calculatedMonth >= 11 && deviation < 12){ // If the person goes over 12 months, it adds 1 to the year and resets the month
             calculatedMonth = 0;
             calculatedYear++;
             getHeading();
             getCalendar();
+            deviation++;
         }
-        else{
+        else if (deviation < 12){
             calculatedMonth++;
             getHeading();
             getCalendar();
+            deviation++;
         }
     });
     $("#previous").click(function(){
-        if (calculatedMonth <= 0){ //Reverse of #next function
+        if (calculatedMonth <= 0 && deviation > -12 ){ //Reverse of #next function
             calculatedMonth = 11;
             calculatedYear--;
             getHeading();
             getCalendar();
+            deviation--;
         }
-        else{
+        else if (deviation > -12){
             calculatedMonth--;
             getHeading();
             getCalendar();
+            deviation--;
         }
     });
     

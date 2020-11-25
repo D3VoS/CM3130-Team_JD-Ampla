538aeeb3661deb7c5359fb47d74f6728d59682bb
osmith1998 <1502307@rgu.ac.uk>
2020-11-25 22:52:02 +0000

    Temporary solution for sending data to the calendar

--- Calendar/views.py
+++ Calendar/views.py
@@ -1,13 +1,41 @@
+from django.db import connection
 from django.shortcuts import render
 from datetime import date, datetime
 
-from .models import event
+from .models import event, bookingEvent
 from Accounts.models import User
-from .forms import createSession, bookingEvent
+from .forms import createSession
 
 # Create your views here.
 def calendar(request):
-    return render(request, 'calendar.html')
+    user = request.user
+    cursor = connection.cursor()
+    cursor.execute(
+        '''
+        SELECT eventName, eventDate, eventStart, eventEnd
+        FROM Calendar_bookingevent, Calendar_event
+        WHERE Calendar_event.id = Calendar_bookingevent.bookingeventid_id AND bookinguser_id = 2 
+        '''
+        )
+    x = cursor.description
+    results = cursor.fetchall()
+    resultsList = []
+    for r in results:
+        i = 0
+        d = {}
+        while i < len(x):
+            d[x[i][0]] = r[i]
+            i = i+1
+        resultsList.append(d)
+
+    bookedEvents = resultsList
+    context = {
+        "events" : bookedEvents
+    }
+    print(context)
+    
+
+    return render(request, 'calendar.html', context)
 
 def session(request):
     user = request.user
@@ -30,7 +58,7 @@ def session(request):
         if request.POST.get('bookingUser') and request.POST.get('bookingEventID'):
             book=bookingEvent()
             book.bookingEventID = event.objects.get(id = request.POST.get('bookingEventID'))
-            book.bookingUser = User.objects.get(id = request.POST.get('bookingUser'))
+            book.bookingUser = user
             book.save()
 
     return render(request, 'session.html', context)
